3b0c63fc102a6c7aa2d519212f4e7c0ba26141c7
Ethan-brau <blochere1394@student.faytechcc.edu>
2024-10-01 13:31:38 -0400

    add

---
+++ p1_lab1_blochere.py
@@ -0,0 +1,20 @@
+
+# CTI 110
+# M1LAB - My First Program
+# Blochere
+# 9/24/24
+print("Woah! Hello!")
+print("This is a python program... \n ")
+print("<|:) \n \n ^ this is a wizard \n")
+print("enter first name ... \n")
+first = input() 
+print("now your last? ... \n ")
+last = input()
+print("nice to meet you! Welcome to CTI - 110!!", first, last)
+print("what do you like?")
+topic = input()
+print("cool!!1! I like", topic, "also! \n")
+print(" \t x \n <|:o \t @ \n \t x \n \n the wizard is looking at his orb...")
+print("how do you feel?")
+emot = input()
+print( emot, ", right?...")
