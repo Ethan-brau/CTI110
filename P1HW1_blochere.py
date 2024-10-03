a0e10ed4262a25b0702eba83f26f40c071dff97d
Ethan-brau <blochere1394@student.faytechcc.edu>
2024-10-03 14:03:24 -0400

    Create P1HW1_blochere.py

---
+++ P1HW1_blochere.py
@@ -0,0 +1,5 @@
+#2 ** 2 is exponent
+# Blocher Ethan
+# 10/03/2024
+# CTI-110 P1HW1
+# today I will be making a simple user friendly exponent and addition/subtraction calculator!, observe notes in between for details (if written in, i get lazy sometimes)
