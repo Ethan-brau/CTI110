760ecd11ef2d96e89b1ac6b8e2dca037e47aa766
Ethan-brau <blochere1394@student.faytechcc.edu>
2024-10-22 13:19:11 -0400

    add

---
+++ P3LAB1_blochere.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#tab in for content of the "if" statement
+# choice = input("front or back?")
+# if choice =="front";
+#   front door code
+# elif choice =="back";
+#   back door code
+# else;
+#   print("i don't know what this is")
