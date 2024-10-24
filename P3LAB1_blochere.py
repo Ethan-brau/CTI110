a193149e6a8aa77d7aec10dc74100fb26bfc0346
Ethan-brau <blochere1394@student.faytechcc.edu>
2024-10-24 17:35:12 +0000

    add

--- P3LAB1_blochere.py
+++ P3LAB1_blochere.py
@@ -1,9 +1,14 @@
+print('\n \n')
 print("===\t=== Welcome to Fork! a text adventure. ===\t===")
-print('\ntype "help" at any time for help!')
+print('\ntype "help" at any time for help!\n')
 
 def main():
     print("Choose Your Own Adventure")
     go_home()
+
+def check_door():
+        print("There is a man at the door... open?")
+        # add options and stuff
 
 def help():
     print("press enter after every choice to submit!")
@@ -26,45 +31,66 @@ def get_pizza():
     print("\nyou got pizza")
     print("the pizza was yummy")
     print('\n*knock knock*... "but the delivery guy already came..."')
-    print("\n =check it out?")
+    print("\n check it out?")
     choice = input()
     if choice == "yes":
         check_door()
     elif choice == "no":
         dont_check()
 
-    def check_door():
-        print("There is a man at the door... open?")
-        # add options and stuff
+   
 
-    def dont_check():
-        print("you sit and enjoy your pizza, prentending noone is home.")
-        print("\nwait.")
-        print("\nwhat was that noise?")
-        print("(open world start...)")
-        choice = input()
+def dont_check():
+    print("you sit and enjoy your pizza, prentending noone is home.")
+    print("\nwait.")
+    print("\nwhat was that noise?")
+    print("(open world start...)")
+    choice = input()
     if choice == "look":
         print("You are in the living room.")
         print('there is a door to the hallway. type "hallway" to enter.')
-        dont_check()
+        livingroom()
     elif choice == "search":
         print('the TV is on, the volume is low...\n the dry warmth of your home brings contrast to the bitter chill outside and the cold touch your windows breathe.')
         print('the blinds are closed and you feel qite secure dispite the noises in the house. likely because of the lock on your livingroom door being dead-bolt.')
         print('your fireplace displays its ability to warm your house. it crackles, the occasional pop of the pine being changed from structure to ash occours.')
         print('your pizza steams, the top of the box reads "Junimos"... ')
         print('not much else of note.\n')
-        dont_check()
+        livingroom()
     elif choice == "help":
         help()
         print('\n \n \n')
-        dont_check()
+        livingroom()
     elif choice =="hallway":
         hallway()
     elif choice =="pizza":
         eat_pizza()
         print('\n')
+
+
 def get_chinese():
     print("\nyou got chinese")
+
+def livingroom()
+    print('you are in the livingroom\n')
+    choice = input()
+    if choice == "look":
+        print("You are in the livingroom.")
+        print('there is a door to the hallway. type "hallway" to enter.\n')
+        livingroom()
+    elif choice == "search":
+        print('the TV is on, the volume is low...\n the dry warmth of your home brings contrast to the bitter chill outside and the cold touch your windows breathe.')
+        print('the blinds are closed and you feel qite secure dispite the noises in the house. likely because of the lock on your livingroom door being dead-bolt.')
+        print('your fireplace displays its ability to warm your house. it crackles, the occasional pop of the pine being changed from structure to ash occours.')
+        print('your pizza steams, the top of the box reads "Junimos"... ')
+        print('not much else of note.\n')
+        livingroom()
+    elif choice == "help":
+        help()
+        print('\n \n \n')
+        livingroom()
+    elif choice =="hallway":
+        hallway()
 
 def hallway():
     print('You are now in the hallway.')
@@ -74,9 +100,14 @@ def hallway():
         print('type "livingroom" for the livingroom. \n"kitchen" for the kitchen \n"bathroom" for bathroom and "bedroom" for bedroom.')
     elif choice =="search":
         print('details about hallway.')
-    
+    elif choice == "help":
+        help()
+        print('\n \n \n')
+        hallway()
+
 def eat_pizza():
-        print('You take a slice, the pizza is delicious! \nthe crust is coated in a buttery garlic sauce, the cheese is a golden brown and the sauce has a sweet, \nbut subtly spicy bite on the tip of your tongue...')
+    print('You take a slice, the pizza is delicious! \nthe crust is coated in a buttery garlic sauce, the cheese is a golden brown and the sauce has a sweet, \nbut subtly spicy bite on the tip of your tongue...')
+    livingroom()
 
 main()
 
