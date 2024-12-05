7fd9f7e43005237f286ab9b9ee0197f783ec1a20
Ethan-brau <blochere1394@student.faytechcc.edu>
2024-12-05 13:50:57 -0500

    add

---
+++ P4HW2_blochere.py
@@ -0,0 +1,102 @@
+# CTI 110
+# P4HW2
+# Blocher E
+# 12/05
+print ("\t\t\t---WELCOME to Ethan's payroll assistant!---")
+print ("please enter an employees name, or enter DONE to terminate.")
+employee1 = input()
+print('How many hours did', employee1, 'work?')
+emp1hours = int(input())
+print('what is', employee1, "'s hourly rate?")
+emp1rate = int(input())
+
+emp1gross = 0
+
+emp1gross = emp1rate * emp1hours
+
+print ("please enter an employees name, or enter DONE to terminate.")
+employee2 = input()
+print('How many hours did', employee2, 'work?')
+emp2hours = input()
+print('what is', employee2, "'s hourly rate?")
+emp2rate = input()
+
+print ("please enter an employees name, or enter DONE to terminate.")
+employee3 = input()
+print('How many hours did', employee3, 'work?')
+emp3hours = input()
+print('what is', employee3, "'s hourly rate?")
+emp3rate = input()
+
+print ("please enter an employees name, or enter DONE to terminate.")
+employee4 = input()
+print('How many hours did', employee4, 'work?')
+emp4hours = input()
+print('what is', employee4, "'s hourly rate?")
+emp4rate = input()
+
+print ("please enter an employees name, or enter DONE to terminate.")
+employee5 = input()
+print('How many hours did', employee5, 'work?')
+emp5hours = input()
+print('what is', employee5, "'s hourly rate?")
+emp5rate = input()
+
+print ("please enter an employees name, or enter DONE to terminate.")
+employee6 = input()
+print('How many hours did', employee6, 'work?')
+emp6hours = input()
+print('what is', employee6, "'s hourly rate?")
+emp6rate = input()
+
+while emp1hours < 1:
+    print('They worked NO hours. please enter an employee with hours.')
+
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+ 
+while emp2hours < 1:
+    print('They worked NO hours. please enter an employee with hours.')
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+ 
+while emp3hours < 1:    
+    print('They worked NO hours. please enter an employee with hours.')
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+ 
+while emp4hours < 1:
+    print('They worked NO hours. please enter an employee with hours.')
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+ 
+while emp5hours < 1:
+    print('They worked NO hours. please enter an employee with hours.')
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+
+while emp6hours < 1:
+    print('They worked NO hours. please enter an employee with hours.')
+    print ("please enter an employees name, or enter DONE to terminate.")
+    employee1 = input()
+    print('How many hours did', employee1, 'work?')
+    emp1hours = input()
+    print('what is', employee1, "'s hourly rate?")
+
+
+
